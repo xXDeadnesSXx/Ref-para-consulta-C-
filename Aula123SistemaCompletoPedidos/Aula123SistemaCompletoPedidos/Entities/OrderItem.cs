f778be8e0d8efb2f19f656a4e70a4eb0c3b62209
xXDeadnesSXx <oibafsegrob2@gmail.com>
2020-12-28 17:08:09 -0300

    Continuação

--- Aula123SistemaCompletoPedidos/Aula123SistemaCompletoPedidos/Entities/OrderItem.cs
+++ Aula123SistemaCompletoPedidos/Aula123SistemaCompletoPedidos/Entities/OrderItem.cs
@@ -1,10 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Aula123SistemaCompletoPedidos.Entities.Enums;
 
 namespace Aula123SistemaCompletoPedidos.Entities
 {
     class OrderItem
     {
+        public int Quantity { get; set; }
+        public double Price { get; set; }
+        public Product Product { get; set; }
+
+        public OrderItem()
+        {
+        }
+
+        public OrderItem(int quantity, double price, Product product)
+        {
+            Quantity = quantity;
+            Price = price;
+            Product = product;
+        }
+
+        public double SubTotal()
+        {
+            return Quantity * Price;
+        }
+
+        public override string ToString()
+        {
+            return
+                Product.Name
+                + ", R$"
+                + Price.ToString("F2")
+                + ", Quantity: "
+                + Quantity
+                + "Subtotal: R$ "
+                + SubTotal().ToString("F2");
+        }
     }
 }
--- Aula123SistemaCompletoPedidos/Aula123SistemaCompletoPedidos/Program.cs
+++ Aula123SistemaCompletoPedidos/Aula123SistemaCompletoPedidos/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Aula123SistemaCompletoPedidos.Entities.Enums;
+using Aula123SistemaCompletoPedidos.Entities;
 
 namespace Aula123SistemaCompletoPedidos
 {
@@ -6,7 +8,44 @@ namespace Aula123SistemaCompletoPedidos
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter cliente data");
+            Console.Write("Name: ");
+            string clientName = Console.ReadLine();
+            
+            Console.Write("Email: ");
+            string email = Console.ReadLine();
+
+            Console.WriteLine("Birth date (DD/MM/YYYY): ");
+            DateTime birthDate = DateTime.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            Console.WriteLine("Enter order data");
+            Console.WriteLine("Status: ");
+            OrderStatus status = Enum.Parse<OrderStatus>(Console.ReadLine());
+
+            Client client = new Client(clientName, email, birthDate);
+            Order order = new Order(DateTime.Now, status, client);
+
+            Console.Write("How many items to this order? ");
+            int n = int.Parse(Console.ReadLine());
+            for(int i = 1; i <= n; i++)
+            {
+                Console.WriteLine("Enter #"+i+" item data:");
+                Console.Write("Product name: ");
+                string productName = Console.ReadLine();
+                Console.Write("Product price: ");
+                double price = double.Parse(Console.ReadLine());
+                Console.Write("Quantity: ");
+                int quantity = int.Parse(Console.ReadLine());
+
+                Product product = new Product(productName, price);
+                OrderItem orderItem = new OrderItem(quantity, price, product);
+                order.AddItem(orderItem);
+            }
+            Console.WriteLine();
+            Console.WriteLine("ORDER SUMMARY:");
+            Console.WriteLine(order);
         }
     }
 }
