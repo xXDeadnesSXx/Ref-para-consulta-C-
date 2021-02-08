d150b154b694fb86f130052cf047c2518a690905
xXDeadnesSXx <oibafsegrob2@gmail.com>
2021-02-07 23:04:39 -0300

    Material Index

--- DCP OMNI/DCP OMNI/Services/MaterialService.cs
+++ DCP OMNI/DCP OMNI/Services/MaterialService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using DCP_OMNI.Data;
+using DCP_OMNI.Models;
 
 namespace DCP_OMNI.Services
 {
@@ -12,6 +13,14 @@ namespace DCP_OMNI.Services
         public MaterialService(DCP_OMNIContext context)
         {
             _context = context;
+        }
+        public List<Material> FindAll()
+        {
+            return _context.Material.OrderBy(x => x.Nome).ToList();
+        }
+        public Material FindById(int id)
+        {
+            return _context.Material.FirstOrDefault(obj=>obj.Id==id);
         }
     }
 }
